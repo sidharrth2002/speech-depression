2ca92ec03c4911f7ced2fa964fa17e4e29851936
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-11-27 16:48:29 +0100

    script

--- scrape_daic_woz.py
+++ scrape_daic_woz.py
@@ -1,21 +1,33 @@
 import bs4
 import requests
+import multiprocessing
+import wget
+import sys
+sys.setrecursionlimit(10000)
+
+url = 'https://dcapswoz.ict.usc.edu/wwwdaicwoz/'
 
 def download_link_to_file(link, file_path):
     with open(file_path, 'wb') as f:
+        print(f'Downloading {link}')
         f.write(requests.get(link).content)
+    # wget.download(link, out=file_path, no_check_certificate=True)
+    return True
 
-url = 'https://dcapswoz.ict.usc.edu/wwwdaicwoz/'
-# download all files in the directory
-r = requests.get(url)
-data = bs4.BeautifulSoup(r.text, 'html.parser')
-for l in data.find_all('a'):
-    # check if parent of l is a td tag
-    if l.get('href') != '/' and l.parent.name == 'td':
-        link = url + l['href']
-        print(f'Downloading {l.get("href")}')
-        print(l.get_text())
-        # get content of a tag
-        download_link_to_file(url + l.get('href'), '/Volumes/Files/monash/daic_woz/' + l.get_text())
+def download(l):
+    return download_link_to_file(url + l.get('href'), '/Volumes/Files/monash/daic_woz/' + l.get_text())
 
+if __name__ == '__main__':
+    # download all files in the directory
+    r = requests.get(url)
+    data = bs4.BeautifulSoup(r.text, 'html.parser')
+    function_arguments = []
+    for l in data.find_all('a'):
+        if l.get('href') != '/' and l.parent.name == 'td' and l.get_text().split('_')[0].isdigit() and int(l.get_text().split('_')[0]) >= 413:
+            link = url + l['href']
+            # get content of a tag
+            function_arguments.append(l)
+
+    pool = multiprocessing.Pool(processes=4)
+    outputs = pool.map(download, function_arguments)
 
