a1492e166dac7c6c8249994858a426362b6287b9
Sidharrth <snag0027@mum-hpc2-login.ib.cluster>
2023-03-27 02:02:26 +0800

    update

--- cluster/dataloader/dataloader.py
+++ cluster/dataloader/dataloader.py
@@ -1,6 +1,10 @@
+# Lint as: python3
+"""DAIC-WOZ Dataset."""
+
 '''
 Custom huggingface dataloader to return audio and labels
 '''
+import json
 import os
 import datasets
 import pandas as pd
@@ -22,14 +26,17 @@ class DaicWozDataset(datasets.GeneratorBasedBuilder):
     '''
     BUILDER_CONFIGS = [
         DaicWozConfig(
-            name="clean",
+            name="daic_woz",
             description="DAIC-WOZ dataset",
         ),
     ]
 
+
     def __init__(self, *args, writer_batch_size=None, split='train', **kwargs):
         super().__init__(*args, writer_batch_size=writer_batch_size, **kwargs)
         self.split = split
+        self.dataset_path = '/home/snag0027/daic_woz'
+
 
     def _info(self):
         return datasets.DatasetInfo(
@@ -38,7 +45,7 @@ class DaicWozDataset(datasets.GeneratorBasedBuilder):
                 {
                     "file": datasets.Value("string"),
                     "audio": datasets.features.Audio(sampling_rate=16_000),
-                    "label": datasets.Value("string"),
+                    "label": datasets.Value("int32"),
                 }
             ),
             supervised_keys=("file", "label"),
@@ -46,34 +53,39 @@ class DaicWozDataset(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager):
         # already downloaded to a specified directory
-        data_dir_train = './daic_woz/train'
-        data_dir_val = './daic_woz/val'
-        data_dir_test = './daic_woz/test'
+        data_dir_train = f'{self.dataset_path}/splits/train'
+        data_dir_val = f'{self.dataset_path}/splits/val'
+        data_dir_test = f'{self.dataset_path}/splits/test'
         if self.split == 'train':
-            data_dir = data_dir_train
+            self.data_dir = data_dir_train
         elif self.split == 'val':
-            data_dir = data_dir_val
+            self.data_dir = data_dir_val
         else:
-            data_dir = data_dir_test
-        if self.config.name == 'clean':
+            self.data_dir = data_dir_test
+        if self.config.name == 'daic_woz':
             train_splits = [
                 datasets.SplitGenerator(
-                    name="train", gen_kwards={"files": data_dir, "name": "train"}
+                    name="train", gen_kwargs={"name": "train"}
                 ),
                 datasets.SplitGenerator(
-                    name="validation", gen_kwards={"files": data_dir, "name": "validation"}
+                    name="validation", gen_kwargs={"name": "validation"}
                 ),
                 datasets.SplitGenerator(
-                    name="test", gen_kwards={"files": data_dir, "name": "test"}
+                    name="test", gen_kwargs={"name": "test"}
                 ),
             ]
         return train_splits
 
-    def _generate_examples(self, files, name):
+    def _generate_examples(self, name):
         key = 0
         examples = []
 
-        audio_dir = "./daic_woz/"
+        if name == "train":
+            audio_dir = f'{self.dataset_path}/splits/train'
+        elif name == "validation":
+            audio_dir = f'{self.dataset_path}/splits/val'
+        else:
+            audio_dir = f'{self.dataset_path}/splits/test'
 
         if not os.path.exists(audio_dir):
             raise FileNotFoundError(
@@ -81,12 +93,63 @@ class DaicWozDataset(datasets.GeneratorBasedBuilder):
             )
 
         # load labels file
-        if name == "train":
-            label_file = os.path.join(audio_dir, "train.csv")
-        elif name == "validation":
-            label_file = os.path.join(audio_dir, "validation.csv")
-        else:
-            label_file = os.path.join(audio_dir, "test.csv")
+        # if name == "train":
+        #     label_file = os.path.join(audio_dir, "train.csv")
+        # elif name == "validation":
+        #     label_file = os.path.join(audio_dir, "validation.csv")
+        # else:
+        #     label_file = os.path.join(audio_dir, "test.csv")
+
+        label_file = os.path.join(self.dataset_path, "labels.csv")
+
+        label_file = pd.read_csv(label_file)
+        # set Participant_ID as index
+        label_file = label_file.set_index("Participant_ID")
+        label_file['PHQ8_Score'] = label_file['PHQ8_Score'].astype('int')
+
+        for folder in os.listdir(audio_dir):
+            # check if folder is a directory and that the first 3 characters are numeric
+            if os.path.isdir(os.path.join(audio_dir, folder)) and folder[:3].isnumeric():
+                for file in os.listdir(os.path.join(audio_dir, folder)):
+                    if file.endswith("PARTICIPANT_merged.wav") and not file.startswith('._'):
+                        print(int(label_file.loc[int(folder[:3])]["PHQ8_Score"]))
+                        examples.append(
+                            {
+                                "file": os.path.join(audio_dir, folder, file),
+                                "audio": os.path.join(audio_dir, folder, file),
+                                # from 300P to 300
+                                "label": int(label_file.loc[int(folder[:3])]["PHQ8_Score"]),
+                            }
+                        )
+
+        with open("data.json", "w") as f:
+            json.dump(examples, f, indent=4)
+
+        for example in examples:
+            yield key, {**example}
+            key += 1
+        examples = []
+
+    def get_data(self, name):
+        key = 0
+        examples = []
+
+        audio_dir = self.dataset_path
+
+        if not os.path.exists(audio_dir):
+            raise FileNotFoundError(
+                f"{audio_dir} does not exist. Make sure you insert the correct path to the audio directory."
+            )
+
+        # load labels file
+        # if name == "train":
+        #     label_file = os.path.join(audio_dir, "train.csv")
+        # elif name == "validation":
+        #     label_file = os.path.join(audio_dir, "validation.csv")
+        # else:
+        #     label_file = os.path.join(audio_dir, "test.csv")
+
+        label_file = os.path.join(audio_dir, "labels.csv")
 
         label_file = pd.read_csv(label_file)
         # set Participant_ID as index
@@ -96,17 +159,19 @@ class DaicWozDataset(datasets.GeneratorBasedBuilder):
             # check if folder is a directory and that the first 3 characters are numeric
             if os.path.isdir(os.path.join(audio_dir, folder)) and folder[:3].isnumeric():
                 for file in os.listdir(os.path.join(audio_dir, folder)):
-                    if file.endswith("PARTICIPANT_merged.wav"):
+                    if file.endswith("PARTICIPANT_merged.wav") and not file.startswith('._'):
                         examples.append(
                             {
                                 "file": os.path.join(audio_dir, folder, file),
                                 "audio": os.path.join(audio_dir, folder, file),
                                 # from 300P to 300
-                                "label": label_file.loc[int(folder[:3])]["PHQ8_Score"],
+                                "label": int(label_file.loc[int(folder[:3])]["PHQ8_Score"]),
                             }
                         )
 
-        for example in examples:
-            yield key, {**example}
-            key += 1
-        examples = []
+        return examples
+
+
+    def __len__(self):
+        return len(self.get_data(self.split))
+    
