aea4c0e8ac1a7c24d5b9c1d5e4442caac85b4ef6
Sidharrth <sidharrth2002@gmail.com>
2023-04-26 23:16:13 +0800

    new training script

---
+++ cluster/train_ast_multistream.py
@@ -0,0 +1,111 @@
+'''
+This script assembles and runs all the models in the cluster.
+It is the only script that is called.
+'''
+
+# TODO: RuntimeError: stack expects each tensor to be equal size, but got [88] at entry 0 and [90] at entry 13
+
+# set current working directory to '/home/snag0027/speech-depression/cluster'
+import os
+
+os.chdir('/home/snag0027/speech-depression/cluster')
+
+import argparse
+from transformers import AutoFeatureExtractor, TrainingArguments, Trainer, AutoProcessor
+from dataloader.dataloadermultimodal import DaicWozDatasetWithFeatures
+from training.utilities import calc_classification_metrics, compute_metrics
+from models.pure_ast import multistream_ast_model
+import logging
+from datasets import load_dataset, DatasetDict
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+import torch
+from config import training_config
+
+logging.basicConfig(level=logging.INFO)
+
+# print available gpus
+logging.info(f"Available GPUs: {torch.cuda.device_count()}")
+# set device to gpu if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# parameters are model_type, epochs and logging file
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_type', type=str, default='ast', choices=['ast', 'handcrafted', 'ast_handcrafted', 'ast_handcrafted_attention'])
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--log_file', type=str, default='log.csv')
+
+args = parser.parse_args()
+
+model = multistream_ast_model(training_config=training_config)
+
+logging.info("Printing model to check")
+logging.info(model)
+
+train_dataset = DaicWozDatasetWithFeatures(split='train')
+validation_dataset = DaicWozDatasetWithFeatures(split='validation')
+
+processor = AutoProcessor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
+feature_extractor = AutoFeatureExtractor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
+
+dataset_config = {
+    "LOADING_SCRIPT_FILES": "/home/snag0027/speech-depression/cluster/dataloader/dataloadermultimodal.py",
+    "CONFIG_NAME": "daic_woz",
+    "DATA_DIR": ".",
+    "CACHE_DIR": "cache_daic_woz",
+}
+
+ds = load_dataset(
+    dataset_config["LOADING_SCRIPT_FILES"],
+    dataset_config["CONFIG_NAME"],
+    data_dir=dataset_config["DATA_DIR"],
+    cache_dir=dataset_config["CACHE_DIR"],
+)
+
+def prepare_dataset(batch):
+    audio_arrays = [x["array"] for x in batch["audio"]]
+    inputs = feature_extractor(audio_arrays, sampling_rate=16000, return_tensors="pt", padding=True, truncation=True)
+    return inputs
+
+# encoded_train = ds["train"].map(prepare_dataset, batched=True, batch_size=4)
+encoded_dataset = ds.map(prepare_dataset, batched=True, batch_size=8)
+
+# print length of each split
+logging.debug("Train length: ", len(ds["train"]))
+logging.debug("Validation length: ", len(ds["validation"]))
+logging.debug("Test length: ", len(ds["test"]))
+
+
+# data collator
+def data_collator(features: List[torch.Tensor]) -> Dict[str, torch.Tensor]:
+    batch = {}
+    # TODO: x["audio_features"] is already a torch.Tensor, fix this
+    logging.debug("Features: {}".format(features[0].keys()))
+    for x in features:
+        if len(x["audio_features"]) != 88:
+            logging.info("Something's wrong")
+            logging.info("Length of audio features: {}".format(len(x["audio_features"])))
+            logging.info(x)
+    batch["audio_features"] = torch.stack([torch.tensor(x["audio_features"]) for x in features])
+    # batch["file"] = [x["file"] for x in features]
+    batch["labels"] = torch.stack([torch.tensor(x["label"]) for x in features])
+    # batch["audio"] = torch.stack([torch.tensor(x["audio"]) for x in features]).to(device)
+    batch["input_values"] = torch.stack([torch.tensor(x["input_values"]) for x in features])
+    return batch
+
+# load model from "./trained_models/ast"
+if args.model_type == 'ast':
+    logging.info("Starting training of pure AST model...")
+    training_args = TrainingArguments(output_dir="./trained_models/ast_multistream_5class", evaluation_strategy="epoch", num_train_epochs=4, per_device_train_batch_size=16, per_device_eval_batch_size=16, gradient_accumulation_steps=16, eval_accumulation_steps=16, logging_steps=6, save_steps=6)
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=encoded_dataset["train"],
+        eval_dataset=encoded_dataset["validation"],
+        compute_metrics=calc_classification_metrics,
+        tokenizer=feature_extractor,
+        data_collator=data_collator,
+    )
+    trainer.train()
+    logging.info("Finished training of multistream AST model.")
+    logging.info("Saved in ./trained_models/ast")
